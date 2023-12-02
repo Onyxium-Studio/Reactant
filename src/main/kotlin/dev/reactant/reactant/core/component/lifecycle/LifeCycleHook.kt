afb6591b24f5c6896f6ef9fdf06fe5925eaaa46a
clayclaw <nope>
2023-12-03 00:53:04 +0800

    remove JvmDefault args

--- src/main/kotlin/dev/reactant/reactant/core/component/lifecycle/LifeCycleHook.kt
+++ src/main/kotlin/dev/reactant/reactant/core/component/lifecycle/LifeCycleHook.kt
@@ -2,15 +2,13 @@ package dev.reactant.reactant.core.component.lifecycle
 
 
 interface LifeCycleHook {
-    @JvmDefault
+
     fun onEnable() {
     }
 
-    @JvmDefault
     fun onDisable() {
     }
 
-    @JvmDefault
     fun onSave() {
     }
 }
